cbc38b652e2c30614f612d137bbe59438d549c98
BuckanianCodes <buckanian@gmail.com>
2025-06-05 09:49:30 +0300

    Styling the step 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { useEffect } from 'react';
 import './App.css'
 import Steps from './Layout/Steps'
 
 function App() {
+ useEffect(() => {
+  const setVH = () => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  };
+
+  setVH();
+  window.addEventListener('resize', setVH);
+
+  return () => {
+    window.removeEventListener('resize', setVH);
+  };
+}, []);
 
 
   return (
